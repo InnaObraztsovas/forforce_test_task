d3c19894aa0f2eb998a6a9e26d08a745ce007c2e
inna <inna.obraztsovas@gmail.com>
2022-12-01 19:04:48 +0200

    test task

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,12 +12,12 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $this->loadUser($manager);
-        $this->loadPhone($manager);
     }
 
-
+//  наповнюєм бд фейковими данними
     public function loadUser(ObjectManager $manager)
     {
+        $operatorCode = [50, 67, 63, 68];
         for ($i = 1; $i <= 2000; $i++){
             $user = new User();
             $user->setName('User ' . $i);
@@ -25,26 +25,21 @@ class AppFixtures extends Fixture
             $date = \DateTime::createFromFormat('d-m-Y', $randomDate);
             $user->setDateOfBirth($date);
             $this->addReference('user_'.$i, $user);
+            for ($a = 1; $a <= rand(1, 3); $a++){
+                $phone = new Phone();
+                /** @var User $user */
+                $user = $this->getReference('user_'.$i);
+                $phone->setUser($user);
+                $phone->setCountryCode('380');
+                $phone->setOperatorCode($operatorCode[array_rand($operatorCode)]);
+                $phone->setNumber(rand(1000000, 9999999));
+                $phone->setBalance(rand(-50, 150));
+                $manager->persist($phone);
+            }
             $manager->persist($user);
         }
 
         $manager->flush();
     }
 
-    public function loadPhone(ObjectManager $manager)
-    {
-        $operatorCode = [50, 67, 63, 68];
-        for ($i = 1; $i <= 3000; $i++) {
-            $phone = new Phone();
-            /** @var User $user */
-            $user = $this->getReference('user_'.rand(1, 2000));
-            $phone->setUser($user);
-            $phone->setCountryCode('380');
-            $phone->setOperatorCode($operatorCode[array_rand($operatorCode)]);
-            $phone->setNumber(rand(1000000, 9999999));
-            $phone->setBalance(rand(-50, 150));
-            $manager->persist($phone);
-            $manager->flush();
-        }
-    }
 }
